dcaf911ff882641b9c3d6ffad928775d730e6081
Hansel Dunlop <hansel@interpretthis.org>
2017-01-18 08:52:44 +0000

    Add additional test for variable number of primes

--- test_table.py
+++ test_table.py
@@ -1,7 +1,7 @@
 from table import multiplied_primes_table
 
 
-def test_multiplied_primes_table():
+def test_multiplied_primes_table_with_three_primes():
     expected_table = [
         [None, 2, 3, 5],
         [2, 4, 6, 10],
@@ -9,3 +9,16 @@ def test_multiplied_primes_table():
         [5, 10, 15, 25]
     ]
     assert multiplied_primes_table(3) == expected_table
+
+
+def test_multiplied_primes_table_with_five_primes():
+    expected_table = [
+        [None, 2, 3, 5, 7, 11],
+        [2, 4, 6, 10, 14, 22],
+        [3, 6, 9, 15, 21, 33],
+        [5, 10, 10, 25, 35, 55],
+        [7, 14, 14, 35, 49, 77],
+        [11, 22, 33, 55, 77, 121],
+    ]
+
+    assert multiplied_primes_table(5) == expected_table
