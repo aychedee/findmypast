dbebf39788f11c22d71164b697249eeaaf17d249
Hansel Dunlop <hansel@interpretthis.org>
2017-01-18 08:39:04 +0000

    Basic test passing

--- table.py
+++ table.py
@@ -1,3 +1,19 @@
 
+
 def multiplied_primes_table(number_of_primes):
-    return []
+    primes = [2, 3, 5]
+    table = [
+        [None] * (number_of_primes + 1)
+        for _ in range(number_of_primes + 1)
+    ]
+
+    for i, prime in enumerate(primes, 1):
+        table[i][0] = prime
+        table[0][i] = prime
+
+    print table
+    for x, factor_x in enumerate(primes, 1):
+        for y, factor_y in enumerate(primes, 1):
+            table[x][y] = factor_x * factor_y
+
+    return table
